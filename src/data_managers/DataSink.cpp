ea0d7c9b6fdad3056b2b3af4e045b2375e63d2c4
Michael <micoraafat96@gmail.com>
2018-12-04 18:20:17 +0200

    StopWaitClient First try

--- src/data_managers/DataSink.cpp
+++ src/data_managers/DataSink.cpp
@@ -4,7 +4,18 @@
  *  Created on: Dec 4, 2018
  *      Author: amrnasr
  */
+#include "DataSink.h"
 
+void DataSink::set_write_file(string path) {
 
+}
+
+void DataSink::feed_next_data(packet_core_data *data) {
+
+}
+
+struct app_data * DataSink:: get_buff() {
+
+}
 
 
--- src/workers/stopWait/StopWaitClient.cpp
+++ src/workers/stopWait/StopWaitClient.cpp
@@ -11,6 +11,7 @@
 #include "../../utils/socketUtils.h"
 #include "../../web_models/packet_utils.h"
 #include "../../web_models/ack_packet.h"
+#include "../../data_managers/DataSink.h"
 
 StopWaitClient::~StopWaitClient() {
 
@@ -19,16 +20,23 @@ StopWaitClient::~StopWaitClient() {
 
 void StopWaitClient::recv_message(int socketFd, DataSink sink) {
 	memset(&src_addr, 0 , sizeof(sockaddr_in));
-	struct packet packet = receive_packet(socketFd, (struct sockaddr *)&src_addr, &error, &time_out);
-	if (error || time_out) {
-
-	} else {
-		struct packet_core_data core_data = extract_pure_data(&packet);
-		sink.feed_next_data(&core_data);
-		struct ack_packet ack_packet = create_ack_packet(packet.seqno);
-		// sending acknowledgment.
-		sendto(socketFd, &ack_packet, sizeof(struct ack_packet*), SOCK_DGRAM,
-				(struct sockaddr *) &src_addr, sizeof(src_addr));
+	bool end = false;
+	while (!end) {
+		struct packet packet = receive_packet(socketFd, (struct sockaddr *)&src_addr, &error, &time_out);
+		if (error || time_out) {
+			cout << "error occurred receiving packet" << endl;
+		} else {
+			if (packet.len != 0) {
+				struct packet_core_data core_data = extract_pure_data(&packet);
+				sink.feed_next_data(&core_data);
+				struct ack_packet ack_packet = create_ack_packet(packet.seqno + packet.len + 1);
+				// sending acknowledgment.
+				sendto(socketFd, &ack_packet, sizeof(struct ack_packet*), SOCK_DGRAM,
+					(struct sockaddr *) &src_addr, sizeof(src_addr));
+			} else {
+				end = true;
+			}
+		}
 	}
 }
 
