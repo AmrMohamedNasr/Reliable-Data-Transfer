34b6829d3ef47b0f2fa805001cc540dee7b64b4c
Amr Mohamed Nasr <re13amr@gmail.com>
2018-12-04 17:09:19 +0200

    empty imp of utils

--- src/utils/socketUtils.cpp
+++ src/utils/socketUtils.cpp
@@ -5,6 +5,16 @@
  *      Author: amrnasr
  */
 
+struct packet receive_packet(int sock, struct sockaddr *src_addr, bool *error, bool*time_out) {
 
+}
+
+bool send_packet(int sock, struct sockaddr *dest_addr, struct packet * pack) {
+
+}
+
+bool hasData(int sock) {
+
+}
 
 
